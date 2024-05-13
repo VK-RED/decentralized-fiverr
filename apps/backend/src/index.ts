32888dc155506885cd7bd62f75d3c9477982989f
VK-RED <vishnukumart96@gmail.com>
2024-05-13 21:09:44 +0530

    remove rootDir

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express';
+import { v1router } from './routers/v1';
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+
+app.use("/v1",v1router);
 
 app.get("/",(req:Request,res:Response)=>{
     console.log(req.body);
