29d2e76a84c5d3509ba6fdf93c2ba76372a823fa
VK-RED <vishnukumart96@gmail.com>
2024-05-14 13:05:16 +0530

    add presignedUrl route

--- apps/backend/src/routers/v1/user.ts
+++ apps/backend/src/routers/v1/user.ts
@@ -1,11 +1,39 @@
 import { Router, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import {db} from "@repo/db/src";
+import {s3} from "@repo/aws/s3";
+import { generateRandomString } from '../../utils';
+
+const bucket = process.env.AWS_BUCKET_NAME as string;
+
 export const userRouter = Router();
+
+const generatePresignedUrl = async()=>{
+    const randomString = generateRandomString();
+    const params = {
+        Bucket: bucket,
+        Key: `/${randomString}/image.png`,
+        Expires: 60 * 5,
+    };
+    const url = s3.getSignedUrl('putObject', params);
+    return url;
+}
+
+userRouter.get("/presignedUrl",async (req,res)=>{
+    // TODO : MAKE THIS AS AN AUTH GET THE REQ.ID FROM IT
+    try {
+        const url = await generatePresignedUrl();
+        return res.json({url});
+    } catch (error) {
+        console.log("Error in Generating Presigned Url --------")
+        console.error(error);
+        throw new Error("Error in Generating Presigned Url");
+    }
+    
+})
 
 userRouter.post("/signin",async(req:Request,res:Response)=>{
     const WalletAddress = "HL3WVRERVREV8REER75VER6"
-
     const user = await db.user.upsert({
         where:{
             address:WalletAddress
