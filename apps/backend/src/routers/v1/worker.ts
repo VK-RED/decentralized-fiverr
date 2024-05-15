ba0e73489b1383792646ae24e156eec59ac9f662
VK-RED <vishnukumart96@gmail.com>
2024-05-15 12:54:55 +0530

    Create SignIn route for worker

--- apps/backend/src/routers/v1/worker.ts
+++ apps/backend/src/routers/v1/worker.ts
@@ -1,7 +1,28 @@
+import { db } from '@repo/db/src';
 import { Router, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 export const workerRouter = Router();
 
-workerRouter.get("/",(req:Request,res:Response)=>{
+
+workerRouter.post("/signin",async(req,res)=>{
+
+    const workerSecret = process.env.WORKER_JWT_SECRET as string;
     
+    //TODO : FIX THE WALLETADDRESS LATER;
+
+    const WalletAddress = "JKN3RJKB13JKJ134K";
+
+    const worker = await db.worker.upsert({
+        where:{
+            address:WalletAddress,
+        },
+        create:{
+            address:WalletAddress,
+        },
+        update:{}
+    });
+
+    const token = jwt.sign({workerId:worker.id},workerSecret,{expiresIn:'24h'});
+    return res.json({token});
 })
