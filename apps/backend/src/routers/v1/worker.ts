74a78d4426f129d87c9684e9745d963398906866
VK-RED <vishnukumart96@gmail.com>
2024-05-15 14:22:30 +0530

    Create nextTask route

--- apps/backend/src/routers/v1/worker.ts
+++ apps/backend/src/routers/v1/worker.ts
@@ -1,6 +1,7 @@
 import { db } from '@repo/db/src';
 import { Router, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import { workerAuth } from '../../middleware';
 
 export const workerRouter = Router();
 
@@ -25,4 +26,35 @@ workerRouter.post("/signin",async(req,res)=>{
 
     const token = jwt.sign({address:worker.address},workerSecret,{expiresIn:'24h'});
     return res.json({token});
+})
+
+// TODO: Test this route heavily once the submitTask is completed
+workerRouter.get("/nextTask",workerAuth,async(req,res)=>{
+    const workerId = req.body.workerId as string;
+
+    //Find a task where the relational submission table should not contain the workerId
+    console.log(workerId);
+    const nextTask = await db.task.findFirst({
+       where:{
+        submissions:{
+            none:{
+                workerId
+            }
+        },
+        completed:false
+       },
+       include:{
+        options:true,
+       },
+    });
+
+    if(!nextTask){
+        return res.json({
+            message:"No Works Left for you !!"
+        })
+    }
+
+    
+    const task = {title:nextTask.title,options:nextTask.options};
+    return res.json({task});
 })
