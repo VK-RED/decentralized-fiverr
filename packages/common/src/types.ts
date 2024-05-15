3f3fdd81aa7263187e214a12668760c04acf2049
VK-RED <vishnukumart96@gmail.com>
2024-05-15 12:38:27 +0530

    Add Types for getTask Route

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -2,3 +2,13 @@ import {z} from "@repo/zod/src";
 import { postTaskSchema } from "./schema";
 
 export type PostTask = z.infer<typeof postTaskSchema>;
+
+export type TaskOptions = Record<number,{count:number,position:number,img_url:string}>;
+
+export interface ResultMessage{
+    message:string;
+}
+
+export interface GetTask{
+    allOptions:TaskOptions,
+}
