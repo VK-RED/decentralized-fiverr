fabe11e8573260dbdfcde13f37688595678e2653
VK-RED <vishnukumart96@gmail.com>
2024-05-16 14:51:42 +0530

    Edit postTaskSchema with amount set to number

--- packages/common/src/schema.ts
+++ packages/common/src/schema.ts
@@ -3,7 +3,7 @@ import {z} from "@repo/zod/src";
 export const postTaskSchema = z.object({
     title : z.string().min(1).max(50).nullable(),
     signature: z.string(),
-    amount: z.string(),
+    amount: z.number(),
     urls: z.string().array(),
 })
 
