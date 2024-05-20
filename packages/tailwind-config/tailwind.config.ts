78d4a893426d7eaa7ee92d4f79f9ef90bc93138f
VK-RED <vishnukumart96@gmail.com>
2024-05-20 11:44:59 +0530

    Setup Tailwind

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,10 +1,13 @@
-module.exports = {
-    content: [
-      "../../packages/ui/**/*.{js,ts,jsx,tsx}",
-      "./**/*.{js,ts,jsx,tsx}",
-      "./app/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
+export default config;
