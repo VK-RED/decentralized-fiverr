11b3be797823d652c5b5f75422b84f81a532ec7f
VK-RED <vishnukumart96@gmail.com>
2024-05-20 11:49:39 +0530

    Initialise web-worker

---
+++ apps/web-worker/postcss.config.mjs
@@ -0,0 +1,10 @@
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// export default config;
+import config from "@repo/tailwind-config/postcss.config.mjs";
+export default config;
---
+++ apps/web-worker/tailwind.config.ts
@@ -0,0 +1,14 @@
+// import type { Config } from "tailwindcss";
+
+// const config: Config = {
+//   content: [
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+// export default config;
+import config from "@repo/tailwind-config/tailwind.config";
+export default config;
