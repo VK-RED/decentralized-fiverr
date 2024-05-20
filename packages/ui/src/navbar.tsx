7593f450b6c79beaf04b8472ebcbb9c8df86457e
VK-RED <vishnukumart96@gmail.com>
2024-05-20 12:57:46 +0530

    Create Navbar

--- packages/ui/src/navbar.tsx
+++ packages/ui/src/navbar.tsx
@@ -1,12 +1,19 @@
-export const Navbar = ({className}:{className?:string}) => {
+'use client';
+import React from "react"
+
+export const Navbar = ({children}:{children:React.ReactNode}) => {
     return(
-        <div className="w-screen border max-h-16 py-1 flex justify-between px-4 items-center">
-            <div className="font-semibold text-xl font-sans cursor-pointer">
-                TUDUM
-            </div>
-            <div className="text-sm cursor-pointer">
-                CONNECT MY WALLET
-            </div>
+        <div>
+            <div className="w-screen border max-h-16 py-1 flex justify-between px-10 items-center">
+                <div className="font-semibold text-xl font-sans cursor-pointer">
+                    TUDUM
+                </div>
+                <div className="text-sm cursor-pointer">
+                    CONNECT MY WALLET
+                </div>
+            </div> 
+            {children}
         </div>
+       
     )
 }
