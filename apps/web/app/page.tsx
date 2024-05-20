d45c00e53bbf45640d0d5764ef438e05397ecad6
VK-RED <vishnukumart96@gmail.com>
2024-05-20 19:05:18 +0530

    Add uploadImage in Home

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,11 @@
 import { Task } from "@repo/ui/task";
+import { UploadImage } from "@repo/ui/uploadImage";
 
 export default function Page(){
   return (
     <div className="flex flex-col items-center">
       <Task className="mt-10"/>
+      <UploadImage className="mt-8"/>
     </div>
   )
 }
