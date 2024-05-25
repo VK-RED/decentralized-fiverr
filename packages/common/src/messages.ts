29ef7fcc8fa2497e5fe115f6da5865662f306d52
VK-RED <vishnukumart96@gmail.com>
2024-05-25 11:18:06 +0530

    feat: create UPLOAD_SUCCESS and TASK_SUCCESS message

--- packages/common/src/messages.ts
+++ packages/common/src/messages.ts
@@ -2,3 +2,5 @@ export const TOTAL_DECIMAL = 1000_000_000;
 
 //TODO: FIX THE BACKEND URL AFTER DEPLOYING THE BACKEND
 export const BACKEND_URL = (process.env.NODE_ENV !== 'development') ? 'https://api.tudum.com/' : 'http://localhost:8000/'
+export const UPLOAD_SUCCESS = `Uploaded the Images Successfully`
+export const TASK_SUCCESS = `Successfully created the task !!`
