5aa02a82f21290c15ffe25e6ae5d76d64747f498
VK-RED <vishnukumart96@gmail.com>
2024-05-25 09:54:01 +0530

    feat: create backend url

--- packages/common/src/messages.ts
+++ packages/common/src/messages.ts
@@ -1,1 +1,4 @@
 export const TOTAL_DECIMAL = 1000_000_000;
+
+//TODO: FIX THE BACKEND URL AFTER DEPLOYING THE BACKEND
+export const BACKEND_URL = (process.env.NODE_ENV !== 'development') ? 'https://api.tudum.com/' : 'http://localhost:8000/'
