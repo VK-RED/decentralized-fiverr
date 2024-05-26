3f322d42042a7a7f1c47115d2fe523fbbfcef9b9
VK-RED <vishnukumart96@gmail.com>
2024-05-26 12:13:25 +0530

    feat: Create NextTask Component

--- apps/web-worker/app/page.tsx
+++ apps/web-worker/app/page.tsx
@@ -1,7 +1,10 @@
+import { NextTask } from "@repo/ui/nextTask";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
       WEB-WORKER
+      <NextTask/>
     </main>
   );
 }
---
+++ packages/ui/src/nextTask.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const NextTask = () => {
+    return (
+        <div>
+            I am next Task
+        </div>
+    )
+}
