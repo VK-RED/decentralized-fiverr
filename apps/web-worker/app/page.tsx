c8ded2e998d18493ae7ee0fd9c8c1cadd2d7a697
VK-RED <vishnukumart96@gmail.com>
2024-05-27 12:03:47 +0530

    chore: cleanup the home page

--- apps/web-worker/app/page.tsx
+++ apps/web-worker/app/page.tsx
@@ -3,7 +3,6 @@ import { NextTask } from "@repo/ui/nextTask";
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      WEB-WORKER
       <NextTask/>
     </main>
   );
