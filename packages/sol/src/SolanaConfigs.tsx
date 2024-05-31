0d488e9c9c22e59aa259b9456eb4737347de129c
Vishnu Kumar <vishnukumart96@gmail.com>
2024-05-31 20:25:32 +0530

    feat: Creat and export Solana Config file

---
+++ packages/sol/src/SolanaConfigs.tsx
@@ -0,0 +1,6 @@
+export { useConnection, useWallet } from '@solana/wallet-adapter-react';
+export { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
+export {
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
