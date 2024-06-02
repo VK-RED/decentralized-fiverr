4726214d54a88b3a22abf283c904e2cf660c1b66
Vishnu Kumar <vishnukumart96@gmail.com>
2024-06-02 18:28:53 +0530

    fix: Solana exports

--- packages/sol/src/SolanaConfigs.tsx
+++ packages/sol/src/SolanaConfigs.tsx
@@ -1,6 +1,3 @@
-export { useConnection, useWallet } from '@solana/wallet-adapter-react';
-export { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
-export {
-    WalletDisconnectButton,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
+export * from '@solana/wallet-adapter-react';
+export * from '@solana/web3.js';
+export * from '@solana/wallet-adapter-react-ui';
