b6b7ca073d86f2db223edd899da9b0352c3b6682
Vishnu Kumar <vishnukumart96@gmail.com>
2024-06-03 20:28:37 +0530

    fix: set isWorkerNav to true for worker layout

--- apps/web-worker/app/layout.tsx
+++ apps/web-worker/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <SolanaProvider>
-          <Navbar isWorkerNav={false}>
+          <Navbar isWorkerNav={true}>
             {children}
           </Navbar>
         </SolanaProvider>
