3d386a9d31fcd10bc4a5ab9704805f29085acecd
Vishnu Kumar <vishnukumart96@gmail.com>
2024-06-03 13:49:32 +0530

    fix: remove amount from PostTask

--- packages/common/src/schema.ts
+++ packages/common/src/schema.ts
@@ -3,7 +3,6 @@ import {z} from "@repo/zod/src";
 export const postTaskSchema = z.object({
     title : z.string().min(1).max(50).nullable(),
     signature: z.string(),
-    amount: z.number(),
     urls: z.string().array(),
 })
 
