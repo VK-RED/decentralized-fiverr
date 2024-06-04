9d433d784b23b2d8b89d4066f6afa3fc24e76852
Vishnu Kumar <vishnukumart96@gmail.com>
2024-06-04 14:44:24 +0530

    feat: add getWorkerBalance state in recoil store

--- packages/store/src/atoms.tsx
+++ packages/store/src/atoms.tsx
@@ -1,9 +1,18 @@
 "use client"
 
-import { NextTaskType } from "@repo/common/types";
+import { GetBalance  } from "@repo/common/types";
 import { atom } from "recoil";
 
 export const workerVerifiedAtom = atom({
     key:'workerVerifiedAtom',
     default:false,
+})
+
+
+export const workerBalAtom = atom<GetBalance>({
+    key:'workerBalanceAtom',
+    default:{
+        availableAmount:0,
+        lockedAmount:0
+    }
 })
--- packages/store/src/selectors.tsx
+++ packages/store/src/selectors.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { selector } from "recoil";
-import { workerVerifiedAtom } from "./atoms";
+import { workerBalAtom, workerVerifiedAtom } from "./atoms";
 
 export const workerVerifiedSelector = selector({
     key:'workerVerifiedSelector',
     get:({get})=>{
         return get(workerVerifiedAtom);
+    }
+})
+
+export const workerBalanceSelector = selector({
+    key:'workerBalanceSelector',
+    get:({get})=>{
+        return get(workerBalAtom);
     }
 })
